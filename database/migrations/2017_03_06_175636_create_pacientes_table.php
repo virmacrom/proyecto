0655bf3e4a785c482719db64a2ae2ad9c9262156
virginia <vmaciasromero@gmail.com>
2017-03-15 16:38:40 +0100

    version 3

---
+++ database/migrations/2017_03_06_175636_create_pacientes_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreatePacientesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('pacientes', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('nombre');
+            $table->string('apellidos');
+            $table->date('fecha_nacimiento');
+            $table->string('direccion');
+            $table->string('telefono');
+            $table->string('enfermedades');
+            $table->timestamps();
+
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('pacientes');
+    }
+}
---
+++ database/migrations/2017_03_13_192945_create_medicos_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateMedicosTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('medicos', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('nombre');
+            $table->string('apellidos');
+            $table->date('fecha_nacimiento');
+            $table->string('direccion');
+            $table->string('telefono');
+            $table->string('especialidad');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('medicos');
+    }
+}
