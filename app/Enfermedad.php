3249bf1866f9bfe06f7af8f0f9f23ec4c609c2c5
Ali <aliciazamorareina6@gmail.com>
2017-03-20 19:12:48 +0100

    sdf

--- app/Enfermedad.php
+++ app/Enfermedad.php
@@ -7,6 +7,6 @@ use Illuminate\Database\Eloquent\Model;
 class Enfermedad extends Model
 {
     public function pacientes(){
-        return $this->belongsToMany('App\paciente');
+        return $this->belongsToMany('App\Paciente');
     }
 }
