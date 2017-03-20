85b68588c478c059a2c7a0ca10b8c797917fae40
virginia <vmaciasromero@gmail.com>
2017-03-20 17:54:10 +0100

    version 3

--- routes/web.php
+++ routes/web.php
@@ -15,5 +15,5 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-Route::resources('medico', 'MedicoController');
-Route::resources('Paciente','PacienteController');
+Route::resource('medico', 'MedicoController');
+Route::resource('Paciente','PacienteController');
