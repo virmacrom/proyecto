9f9355ec01211db772d49ee984b94759f20e3973
virginia <vmaciasromero@gmail.com>
2017-04-01 14:24:04 +0200

    version 4

--- app/Http/Controllers/PacienteController.php
+++ app/Http/Controllers/PacienteController.php
@@ -64,6 +64,16 @@ class PacienteController extends Controller
         return redirect()->route('pacientes.index');
     }
 
+    public function validar_dni($dni){
+        $letra = substr($dni, -1);
+        $numeros = substr($dni, 0, -1);
+        if ( substr("TRWAGMYFPDXBNJZSQVHLCKE", $numeros%23, 1) == $letra && strlen($letra) == 1 && strlen ($numeros) == 8 ){
+            echo 'valido';
+        }else{
+            echo 'no valido';
+        }
+    }
+
     /**
      * Display the specified resource.
      *
