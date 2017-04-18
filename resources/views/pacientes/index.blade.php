7031c0db6cafebf65012085a57d6af6b1380a2d5
Ali <aliciazamorareina6@gmail.com>
2017-04-18 13:41:01 +0200

    Creación de vistas de medico y paciente

--- resources/views/pacientes/index.blade.php
+++ resources/views/pacientes/index.blade.php
@@ -5,38 +5,40 @@
         <div class="row">
             <div class="col-md-8 col-md-offset-2">
                 <div class="panel panel-default">
-                    <div class="panel-heading">Medicos</div>
+                    <div class="panel-heading">Pacientes</div>
 
                     <div class="panel-body">
                         @include('flash::message')
-                        {!! Form::open(['route' => 'medicos.create', 'method' => 'get']) !!}
-                        {!!   Form::submit('Crear medico', ['class'=> 'btn btn-primary'])!!}
+                        {!! Form::open(['route' => 'pacientes.create', 'method' => 'get']) !!}
+                        {!!   Form::submit('Crear paciente', ['class'=> 'btn btn-primary'])!!}
                         {!! Form::close() !!}
 
                         <br><br>
                         <table class="table table-striped table-bordered">
                             <tr>
-                                <th>Codigo</th>
-                                <th>Consulta</th>
-                                <th>Especialidad</th>
+
+                                <th>Nuhsa</th>
+                                <th>Dni</th>
+                                <th>Operado</th>
+
                                 <th colspan="2">Acciones</th>
                             </tr>
 
-                            @foreach ($medicos as $medico)
+                            @foreach ($pacientes as $paciente)
 
 
                                 <tr>
-                                    <td>{{ $medico->code }}</td>
-                                    <td>{{ $medico->consulta }}</td>
-                                    <td>{{ $medico->especialidad->name }}</td>
 
+                                    <td>{{ $paciente->nuhsa }}</td>
+                                    <td>{{ $paciente->dni }}</td>
+                                    <td>{{ $paciente->operado }}</td>
                                     <td>
-                                        {!! Form::open(['route' => ['medicos.edit',$medico->id], 'method' => 'get']) !!}
+                                        {!! Form::open(['route' => ['pacientes.edit',$paciente->id], 'method' => 'get']) !!}
                                         {!!   Form::submit('Editar', ['class'=> 'btn btn-warning'])!!}
                                         {!! Form::close() !!}
                                     </td>
                                     <td>
-                                        {!! Form::open(['route' => ['medicos.destroy',$medico->id], 'method' => 'delete']) !!}
+                                        {!! Form::open(['route' => ['pacientes.destroy',$paciente->id], 'method' => 'delete']) !!}
                                         {!!   Form::submit('Borrar', ['class'=> 'btn btn-danger' ,'onclick' => 'if(!confirm("¿Está seguro?"))event.preventDefault();'])!!}
                                         {!! Form::close() !!}
 
@@ -48,5 +50,4 @@
                 </div>
             </div>
         </div>
-    </div>
 @endsection
