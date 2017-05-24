41963f6c62ec6bd094896187d253c520cda5e1e6
virginia <vmaciasromero@gmail.com>
2017-05-24 11:13:16 +0200

    crear middleware

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -53,7 +53,7 @@ class Kernel extends HttpKernel
         'can' => \Illuminate\Auth\Middleware\Authorize::class,
         'guest' => \App\Http\Middleware\RedirectIfAuthenticated::class,
         'throttle' => \Illuminate\Routing\Middleware\ThrottleRequests::class,
-        'medico' => \App\Http\Middleware\Medico::class,
+        'encuesta' => \App\Http\Middleware\Encuesta::class,
         'paciente' => \App\Http\Middleware\Paciente::class,
 
     ];
