5f93f3d2dda846b8a75d96e47f55d99c420d5843
virginia <vmaciasromero@gmail.com>
2017-05-26 12:32:04 +0200

    quitados los _

--- database/migrations/2017_03_20_173615_create_citas_table.php
+++ database/migrations/2017_03_20_173615_create_citas_table.php
@@ -4,7 +4,7 @@ use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class CreatePacienteEnfermedadsTable extends Migration
+class CreateCitasTable extends Migration
 {
     /**
      * Run the migrations.
@@ -13,12 +13,14 @@ class CreatePacienteEnfermedadsTable extends Migration
      */
     public function up()
     {
-        Schema::create('paciente_enfermedads', function (Blueprint $table) {
+        Schema::create('cita', function (Blueprint $table) {
             $table->increments('id');
+            $table->date('fechacita');
+            $table->unsignedInteger('medico_id');
+            $table->foreign('medico_id')->references('id')->on('medicos')->onDelete('cascade');
             $table->unsignedInteger('paciente_id');
             $table->foreign('paciente_id')->references('id')->on('pacientes')->onDelete('cascade');
-            $table->unsignedInteger('enfermedad_id');
-            $table->foreign('enfermedad_id')->references('id')->on('enfermedads')->onDelete('cascade');
+
             $table->timestamps();
         });
     }
@@ -30,6 +32,6 @@ class CreatePacienteEnfermedadsTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('paciente_enfermedads');
+        Schema::dropIfExists('cita');
     }
 }
